4594b8fb76ef70d8bf323d7a326aeec62e93cd6c
julianafreiitas <juliana.freitasmk2@gmail.com>
2026-09-16 23:11:29 -0300

    user

---
+++ app/useEffect/page.jsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { div } from 'motion/react-client';
+import './dummy.css'
+import { pegardados } from './function';
+
+export default function Usuarios() {
+
+    const {listaUsers, msgErro} = pegardados()
+
+    return (
+        <main>
+        
+                <h1>Página de usuários</h1>
+                {msgErro != "" && <p>{msgErro}</p>}
+
+                {listaUsers.length > 0 ?
+                    //se tiver Usuario mostra essa div
+                    <div className='espaco'>
+                        {listaUsers.map((user) => {
+                            return (
+                                
+                                <div className="informa" >
+                                    <h3>{user.fircstName}{" "}{user.lastName}</h3>
+                                    <img src={user.image} />
+                                    <p>E-mail: <b>{user.email}</b></p>
+                                    <p>Sexualidade: {user.gender}</p>
+                                </div>
+                              
+                            )
+                        })}
+                    </div>
+                    :
+                    //se não tiver usuario mostra isso
+                    <div>
+                        sem Usuarios por enquanto! Tente novamente mais tarde...
+                    </div>}
+                    
+
+        </main>
+    )
+}
